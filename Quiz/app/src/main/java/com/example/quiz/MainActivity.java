3a97a74f4156295c7c294d49c5ae1d8f5d11a900
Александр Паршин <parshin_sashek@mail.ru>
2019-10-06 12:28:46 +0300

    Игра Викторина
    Добавил стили.
    Убрал заголовок приложения.
    Убрал строку состояния.

---
+++ Quiz/app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //убираем строку состояния андроида
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+}
