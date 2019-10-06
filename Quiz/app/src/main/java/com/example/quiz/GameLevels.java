a2d61b372caf9b40199d8e1484a0d8465d8e37f3
Александр Паршин <parshin_sashek@mail.ru>
2019-10-06 22:05:20 +0300

    Добавление Activity GameLevels
    Созданы стили для кнопок игровых уровней.
    Добавлена сама активность на которой добавлены 30 кнопок с уровнями и кнопка назад.

---
+++ Quiz/app/src/main/java/com/example/quiz/GameLevels.java
@@ -0,0 +1,22 @@
+package com.example.quiz;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_levels);
+
+
+        //убираем строку состояния андроида
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+}
